a9995bbf9a4a2fe081c29520e1bcbf3d6b3939de
edocsitahw <2257699870@qq.com>
2026-04-20 21:28:45 +0800

    build: 初始化后端框架

---
+++ code/backend/diligence/src/main/java/org/nfupt4/diligence/DiligenceApplication.java
@@ -0,0 +1,13 @@
+package org.nfupt4.diligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiligenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiligenceApplication.class, args);
+    }
+
+}
---
+++ code/backend/diligence/src/test/java/org/nfupt4/diligence/DiligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nfupt4.diligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiligenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
